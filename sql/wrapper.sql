9c81c19ebbfa21effbeb97b853cb151eeaeae76c
Barrett Otte <Barrett.Otte@goodville.com>
2019-02-28 16:43:26 -0500

    .

--- sql/wrapper.sql
+++ sql/wrapper.sql
@@ -7,19 +7,33 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
+-- TODO --------
+--   Python: Replace newline chars with &!NL!&, SQL: replace with CHAR(13)+CHAR(10)
+--   Python: Replace tab chars with &!TAB!&, SQL: replace with CHAR(9)
+----------------
+
+CREATE OR ALTER PROCEDURE [dbo].[SQLUnit_Wrapper](
+	@SQL_STRING NVARCHAR(MAX),
+	@RES_COLS NVARCHAR(3000)
+)  
+WITH EXECUTE AS CALLER 
+AS
 BEGIN
 	SET NOCOUNT ON
 
-	-- TODO : Pass as params -- 
-	DECLARE @SQL_STRING AS NVARCHAR(MAX) = N'';
-	DECLARE @RES_COLS AS NVARCHAR(MAX) = N'';
-	SET @SQL_STRING = N'INSERT INTO [BARRETT_TEST].[dbo].[roles] (role_name) VALUES (&!SQ!&TEST&!SQ!&); SELECT * FROM [BARRETT_TEST].[dbo].[roles];';
+	-- TODO : Pass as params from Python side ------
+	-- SQL_STRING : Load from {test}.sql, sanitize, and replace single quotes with '&!SQ!&'
+	-- RES_COLS   : Load from {test}.expected.json - '|,|' delimited list of columns with types
+	SET @SQL_STRING = N'
+		INSERT INTO [BARRETT_TEST].[dbo].[roles] (role_name) VALUES (&!SQ!&TEST&!SQ!&); 
+		SELECT * FROM [BARRETT_TEST].[dbo].[roles];
+	';
 	SET @RES_COLS = N'id VARCHAR(50)|,|result VARCHAR(50)|,|';
-
+	--------------------------------------------
 
 	DECLARE @POS INT = 0;
 	DECLARE @LEN INT = 0;
-	DECLARE @VAL VARCHAR(2000);
+	DECLARE @VAL VARCHAR(255);
 	DECLARE @DELIM_COL VARCHAR(3) = '|,|';
 
 
@@ -41,7 +55,7 @@ BEGIN
 	BEGIN TRANSACTION SQLUnit_Wrapper
 		SAVE TRANSACTION SQLUnit_Wrapper
 		BEGIN TRY
-			SET @SQL_STRING = REPLACE(@SQL_STRING, '&!SQ!&', CHAR(39));
+			SET @SQL_STRING = 'BEGIN ' + REPLACE(@SQL_STRING, '&!SQ!&', CHAR(39)) + ' END';
 			EXEC('CREATE OR ALTER PROCEDURE [dbo].[SQLUnit_TmpSP] AS ' + @SQL_STRING);
 			INSERT INTO #TMP EXEC [dbo].[SQLUnit_TmpSP];
 			SELECT * FROM #TMP;
@@ -49,10 +63,11 @@ BEGIN
 			DROP TABLE #TMP;
 		END TRY
 		BEGIN CATCH
+			DECLARE @EXEC_FAILED BIT = 1;
 			SELECT
+				@EXEC_FAILED AS __SQLUnit_FAILED__,  -- Quick execution fail check for Python side
 				@SQL_STRING AS SQL_STRING,
 				@RES_COLS AS RES_COLS,
-				@DELIM_COL AS DELIM_COL,
 				ERROR_NUMBER() AS ErrorNumber,
 				ERROR_SEVERITY() AS ErrorSeverity,
 				ERROR_STATE() AS ErrorState,
